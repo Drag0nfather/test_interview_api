2a43373f47dbd4ceb867a39aa580b615d3cd2c12
Andrew <Andrey18651@gmail.com>
2021-07-08 20:08:30 +0300

    add QuestionSerializer, add id field in InterviewSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from api.models import Interview
+from api.models import Interview, Question
 
 
 class InterviewSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('name', 'start_date', 'end_date', 'description')
+        fields = ('id', 'name', 'start_date', 'end_date', 'description')
         model = Interview
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'text', 'type', 'interview')
+        model = Question
