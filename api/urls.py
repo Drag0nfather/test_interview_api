3c7986327d1ae3af080e71b6f48ff32638b12e7d
Andrew <Andrey18651@gmail.com>
2021-07-08 22:00:43 +0300

    add active_interviews route ro urls

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
-from .views import InterviewViewSet, QuestionViewSet
+from .views import InterviewViewSet, QuestionViewSet, ActiveInterviewViewSet
 
 router = DefaultRouter()
 
 router.register('interviews', InterviewViewSet)
 router.register('interviews/(?P<id>\d+)/questions', QuestionViewSet)
+router.register('active_interviews', ActiveInterviewViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
