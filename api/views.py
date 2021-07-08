a65bc5183cefa5c4021b6b0d81916a729c03f2a4
Andrew <Andrey18651@gmail.com>
2021-07-08 21:59:09 +0300

    add ActiveInterviewViewSet

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
+from .filters import actual_interviews
 from .models import Interview, Question
 from .permissions import IsAdminOrReadOnly
 from .serializers import InterviewSerializer, QuestionSerializer
@@ -18,9 +20,15 @@ class QuestionViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
 
     def get_queryset(self):
-        interview = get_object_or_404(Interview, id=self.kwargs['id'])
+        interview = get_object_or_404(Interview, id=self.kwargs.get('id'))
         return interview.questions.all()
 
     def perform_create(self, serializer):
-        interview = get_object_or_404(Interview, pk=self.kwargs['id'])
+        interview = get_object_or_404(Interview, pk=self.kwargs.get('id'))
         serializer.save(interview=interview)
+
+
+class ActiveInterviewViewSet(viewsets.ModelViewSet):
+    queryset = actual_interviews()
+    serializer_class = InterviewSerializer
+    permission_classes = [AllowAny]
