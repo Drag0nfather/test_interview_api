c773e30bfd78c08c2b0eaab2d86e22285989eaf2
Andrew <Andrey18651@gmail.com>
2021-07-08 18:47:59 +0300

    change DateTime field to Date in Interview model

--- api/models.py
+++ api/models.py
@@ -6,8 +6,8 @@ User = get_user_model()
 
 class Interview(models.Model):
     name = models.CharField(max_length=50)
-    start_date = models.DateTimeField(auto_now_add=True)
-    end_date = models.DateTimeField()
+    start_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField()
     description = models.TextField(max_length=300)
 
     def __str__(self):
