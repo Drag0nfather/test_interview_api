9db3b0e154d2b7327bc3ba179e5408a1a073190b
Andrew <Andrey18651@gmail.com>
2021-07-08 17:52:23 +0300

    add Interview model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Interview(models.Model):
+    name = models.CharField(max_length=50)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField()
+    description = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.name
