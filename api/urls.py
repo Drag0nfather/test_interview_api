67fcbe86d80c9f3a53b7343c7ee41cc76f2a441e
Andrew <Andrey18651@gmail.com>
2021-07-08 18:48:23 +0300

    add reviews endpoint in api/urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
 
+from .views import InterviewViewSet
+
+router = DefaultRouter()
+
+router.register('reviews', InterviewViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('token/', views.obtain_auth_token),
 ]
