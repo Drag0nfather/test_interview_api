721dca8ad660444863b86ae2cd5e370cb77db24d
Andrew <Andrey18651@gmail.com>
2021-07-08 21:13:19 +0300

    add IsAdminOrReadOnly permission class in Interview and Question ViewSet

--- api/views.py
+++ api/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
 from .models import Interview, Question
+from .permissions import IsAdminOrReadOnly
 from .serializers import InterviewSerializer, QuestionSerializer
 
 
 class InterviewViewSet(viewsets.ModelViewSet):
     queryset = Interview.objects.all()
     serializer_class = InterviewSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
     def get_queryset(self):
         interview = get_object_or_404(Interview, id=self.kwargs['id'])
