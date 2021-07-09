69590e07cab87037649aef31f12e6af8790b26e6
Andrew <Andrey18651@gmail.com>
2021-07-09 14:03:34 +0300

    add OptionViewSet

--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAdminUser
 
 from .filters import actual_interviews
-from .models import Interview, Question
+from .models import Interview, Question, Option
 from .permissions import IsAdminOrReadOnly
-from .serializers import InterviewSerializer, QuestionSerializer
+from .serializers import InterviewSerializer, QuestionSerializer, OptionSerializer
 
 
 class InterviewViewSet(viewsets.ModelViewSet):
@@ -32,3 +32,20 @@ class ActiveInterviewViewSet(viewsets.ModelViewSet):
     queryset = actual_interviews()
     serializer_class = InterviewSerializer
     permission_classes = [AllowAny]
+
+
+class OptionViewSet(viewsets.ModelViewSet):
+    queryset = Option.objects.all()
+    serializer_class = OptionSerializer
+    permission_classes = [IsAdminUser]
+
+    def perform_create(self, serializer):
+        question = get_object_or_404(Question,
+                                     pk=self.kwargs.get('question_pk'),
+                                     interview__id=self.kwargs.get('id'))
+        serializer.save(question=question)
+
+    def get_queryset(self):
+        question = get_object_or_404(Question,
+                                     id=self.kwargs.get('question_pk'))
+        return question.options.all()
