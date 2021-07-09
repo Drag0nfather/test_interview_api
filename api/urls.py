f5d4758340b56361859904f794b4be8528e08627
Andrew <Andrey18651@gmail.com>
2021-07-09 14:05:50 +0300

    add options url

--- api/urls.py
+++ api/urls.py
@@ -2,13 +2,17 @@ from django.urls import path, include
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
-from .views import InterviewViewSet, QuestionViewSet, ActiveInterviewViewSet
+from .views import InterviewViewSet, QuestionViewSet, ActiveInterviewViewSet, OptionViewSet
 
 router = DefaultRouter()
 
 router.register('interviews', InterviewViewSet)
 router.register('interviews/(?P<id>\d+)/questions', QuestionViewSet)
 router.register('active_interviews', ActiveInterviewViewSet)
+router.register(
+    'interviews/(?P<id>\d+)/questions/(?P<question_pk>\d+)/options',
+    OptionViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
