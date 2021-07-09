1b4841c5b76007d1e07f955b4b7de53bf9bf899a
Andrew <Andrey18651@gmail.com>
2021-07-09 14:05:18 +0300

    add OptionSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from api.models import Interview, Question
+from api.models import Interview, Question, Option
 
 
 class InterviewSerializer(serializers.ModelSerializer):
@@ -13,3 +13,10 @@ class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'text', 'type', 'interview')
         model = Question
+
+
+class OptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'name', 'question')
+        model = Option
+
