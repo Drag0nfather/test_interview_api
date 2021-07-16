3202bdb8f210183f7f20b2d36701899788ff9a8c
Andrew <Andrey18651@gmail.com>
2021-07-16 19:05:27 +0300

    add urls: 1) to create answer, 2) to get user answers

--- api/urls.py
+++ api/urls.py
@@ -1,24 +1,28 @@
+from django.conf.urls import url
 from django.urls import path, include
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
-from .views import InterviewViewSet, QuestionViewSet, \
-    ActiveInterviewViewSet, OptionViewSet, AnswerViewSet
+from .views import (InterviewViewSet,
+                    QuestionViewSet,
+                    ActiveInterviewViewSet,
+                    AnswerViewSet,
+                    UserAnswersViewSet)
 
 router = DefaultRouter()
 
 router.register('interviews', InterviewViewSet)
 router.register('interviews/(?P<id>\d+)/questions', QuestionViewSet)
 router.register('active_interviews', ActiveInterviewViewSet)
-router.register(
-    'interviews/(?P<id>\d+)/questions/(?P<question_pk>\d+)/options',
-    OptionViewSet)
-router.register(
-    'interviews/(?P<id>\d+)/questions/(?P<question_pk>\d+)/answers',
-    AnswerViewSet)
 
 
 urlpatterns = [
+    url(r'interviews/(?P<interview_id>\d+)/questions/(?P<question_id>\d+)/answers',
+        AnswerViewSet.as_view({'post': 'create_answer'})),
+
+    url(r'interviews/(?P<interview_id>\d+)/answers/(?P<user_id>\d+)',
+        UserAnswersViewSet.as_view({'get': 'get_passed_interviews'})),
+
     path('', include(router.urls)),
     path('token/', views.obtain_auth_token),
 ]
