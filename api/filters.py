42caea17a28781bb088e5ac8ca3b198fccee48ec
Andrew <Andrey18651@gmail.com>
2021-07-16 18:57:18 +0300

    rename filter name

--- api/filters.py
+++ api/filters.py
@@ -3,5 +3,5 @@ from datetime import datetime
 from api.models import Interview
 
 
-def actual_interviews():
+def get_actual_interviews():
     return Interview.objects.filter(end_date__gte=datetime.today())
