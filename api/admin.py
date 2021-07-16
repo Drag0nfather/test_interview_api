a764907b431fea0610210e1bf859f12013dce2a4
Andrew <Andrey18651@gmail.com>
2021-07-16 18:56:19 +0300

    delete Option model from admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from api.models import Option, Question, Interview, Answer
+from api.models import Question, Interview, Answer
 
 
 class AuthorAdmin(admin.ModelAdmin):
     pass
 
 
-admin.site.register(Option)
 admin.site.register(Question)
 admin.site.register(Interview)
 admin.site.register(Answer)
